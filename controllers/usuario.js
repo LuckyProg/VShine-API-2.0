211edb5a7b83bd35560fbce804bdd066b38c726d
servin <tarzanboycraft@hotmail.com>
2018-09-18 13:46:33 -0500

    correción de register/usuario y verificacion de login

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,4 +1,4 @@
-'use stinct'
+'use strict'
 var bcrypt = require('bcrypt-nodejs');
 
 var User = require('../models/usuario');
@@ -6,68 +6,89 @@ var Auto = require('../models/auto');
 
 
 function login(req, res){
-    res.status(200).send({
-        mensaje: 'hola desde usuario'
+    var params = req.body;
+
+    var correo = params.correo;
+    var pass = params.pass;
+
+    User.findOne({correo: correo}, (err, user) => {
+        if(err) return res.status(500).send({mensaje: 'Error en la petición'});
+
+        if(user){
+            bcrypt.compare(pass, user.pass, (err, check) => {
+                if(check){
+                    return res.status(200).send({usuario: user});
+                }else{
+                    return res.status(404).send({mensaje: 'Usuario no se ha podido identificar'});
+                }
+            });
+        }else{
+            return res.status(404).send({mensaje: 'Usuario no se ha podido identificar!!'});
+        }
     });
 }
 
 function saveUser(req, res){
     var params = req.body;
 
-    if(params.nombre &&
-        params.correo &&
-        params.celular &&
-        params.pass &&
-        params.placa &&
-        params.modelo && 
-        params.color &&
-        params.marca){
+    if(params.nombre && params.correo && params.celular && params.pass && params.placa && params.modelo &&  params.color && params.marca){
 
             var user = new User();
             user.nombre = params.nombre;
             user.correo = params.correo;
             user.celular = params.celular;
 
+            var auto = new Auto({
+                placa: params.placa,
+                modelo: params.modelo,
+                color: params.color,
+                marca: params.marca,
+            });
+
             //DUPLICADOS
-            User.find({correo: user.correo.toLowerCase()}).exec((err, users) => {
-                if(err) return res.status(500).send({mensaje: 'Error en la petición de usuarios'});
-                if(users && users.length >= 1) return res.status(200).send({mensaje: 'El correo ingresado ya tiene una cuenta registrada'});
+            User.find({correo: user.correo}).exec((err, users) => {
+                if(err){
+                    res.status(500).send({mensaje: 'Error en la petición de usuarios'});
+                }else if(users && users.length >= 1){
+                    res.status(200).send({mensaje: 'El correo ingresado ya tiene una cuenta registrada'});
+                }else{
+                    Auto.find({placa: params.placa}).exec((err, autos) => {
+                        if(err){
+                            res.status().send({mensaje: 'Error en la petición de autos'});
+                        }else if(autos && autos.length >= 1){
+                            
+                            res.status(200).send({mensaje: 'Placas ya registradas'});
+                        }else{
+                            bcrypt.hash(params.pass, null, null, (err, hash) => {
+                                user.pass = hash;
+                                user.save((err, userStored) => {
+                                    if(err){
+                                        res.status(500).send({mensaje: 'Error al guardar usuario'});
+                                    }else if(userStored){
+                                        auto.usuario = userStored._id;   
+                                        auto.save((err)=>{
+                                            if(err){
+                                                res.status(500).send({mensaje: 'Error al guardar auto de usuario'});
+                                            }else{
+                                                res.status(200).send({usuario: userStored});
+                                            }
+                                        });
+                                        
+                                    }else{
+                                        res.status(404).send({mensaje: 'No se ha registrado el usuario'});
+                                    }
+                                })
+                            }); 
+                        }
+                    });
+                }
+                
+                
             });
-            Auto.find({placa: params.placa.toLowerCase()}).exec((err, autos) => {
-                if(err) return res.status().send({mensaje: 'Error en la petición de autos'});
-                if(autos && autos.length >= 1) return res.status(200).send({mensaje: 'Placas ya registradas'});
-            });
-            bcrypt.hash(params.pass, null, null, (err, hash) => {
-                user.pass = hash;
-                user.save((err, userStored) => {
-                    if(err) return res.status(500).send({mensaje: 'Erorr al guardar usuario'});
-                    if(userStored){
-                        var auto = new Auto({
-                            placa: params.placa,
-                            modelo: params.modelo,
-                            color: params.color,
-                            marca: params.marca,
-                            usuario: userStored._id
-                        });
-                        auto.save((err)=>{
-                            if(err) return res.status(500).send({mensaje: 'Error al guardar auto de usuario'});
-                        });
-                        res.status(200).send({usuario: userStored});
-                    }else{
-                        res.status(404).send({mensaje: 'No se ha registrado el usuario'});
-                    }
-                })
-            }); 
-            
-            
-                       
-            
 
 
     }else{
-        res.status(200).send({
-            mensaje: 'Datos incompletos para registrar usuario'
-        });
+        res.status(200).send({ mensaje: 'Datos incompletos para registrar usuario'});
     }
 
 
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ var express = require('express');
 var UserController = require('../controllers/usuario');
 
 var api = express.Router();
-api.get('/login', UserController.login);
+api.post('/login', UserController.login);
 api.post('/register', UserController.saveUser);
 
 module.exports = api;
