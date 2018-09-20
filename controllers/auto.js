6a126c211281f691eed8a39be7a12a6964e483d2
servin <tarzanboycraft@hotmail.com>
2018-09-19 21:08:09 -0500

    controleres inicio de auto y lavados

---
+++ controllers/auto.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var bcrypt = require('bcrypt-nodejs');
+
+var Auto = require('../models/auto');
+var Usuario = require('../models/usuario');
+
+function registerAuto(req, res){
+
+}
+module.exports = {
+    registerAuto
+};
---
+++ controllers/lavado.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var bcrypt = require('bcrypt-nodejs');
+
+var Auto = require('../models/auto');
+var Usuario = require('../models/usuario');
+var Lavadi = require('../models/lavado');
+
+function registerAuto(req, res){
+
+}
+module.exports = {
+    registerAuto
+};
